de75d2d53c095062c8860f36d560e76df7d1574c
dwidianayup <111201509001@mhs.dinus.ac.id>
2018-03-05 05:49:30 +0700

    update layout

--- app/src/main/java/com/adwidian/ramaniapi/projectkp/DetailMapsActivity.java
+++ app/src/main/java/com/adwidian/ramaniapi/projectkp/DetailMapsActivity.java
@@ -1,11 +1,19 @@
 package com.adwidian.ramaniapi.projectkp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 
 import com.ujuizi.ramani.mapsapi.MapEventListener;
@@ -30,7 +38,7 @@ import org.oscim.tiling.source.bitmap.DefaultSources;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DetailMapsActivity extends AppCompatActivity implements RMAccountManager.RMAccountManagerListener {
+public class DetailMapsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, RMAccountManager.RMAccountManagerListener {
 
     private MapView mMapView;
     private Map mMap;
@@ -47,6 +55,7 @@ public class DetailMapsActivity extends AppCompatActivity implements RMAccountMa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setLatLong();
 
@@ -64,6 +73,15 @@ public class DetailMapsActivity extends AppCompatActivity implements RMAccountMa
         listPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         permissionHelper.checkAndRequestPermissions(listPermissions);
 
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout4);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void onPermissionDone(int REQUEST_CODE) {
@@ -175,5 +193,64 @@ public class DetailMapsActivity extends AppCompatActivity implements RMAccountMa
         this.latitude = getIntent().getDoubleExtra("latitude",0);;
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout4);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.maps) {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.list_marker) {
+            Intent intent = new Intent(this,RecycleViewActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.about) {
+            Intent intent = new Intent(this,AboutActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.auth_logout) {
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout4);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
