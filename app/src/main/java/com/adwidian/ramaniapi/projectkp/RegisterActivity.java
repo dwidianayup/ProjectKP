a6825d1134c5ad89c7cf0641741a6e41c8ece53a
ahmadbasir <akhmadbasir5@gmail.com>
2018-03-08 01:37:20 +0700

    register service done

--- app/src/main/java/com/adwidian/ramaniapi/projectkp/RegisterActivity.java
+++ app/src/main/java/com/adwidian/ramaniapi/projectkp/RegisterActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 
 import com.adwidian.ramaniapi.projectkp.entities.AccessToken;
 import com.adwidian.ramaniapi.projectkp.entities.ApiError;
 import com.adwidian.ramaniapi.projectkp.network.ApiService;
 import com.adwidian.ramaniapi.projectkp.network.RetrofitBuilder;
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +35,8 @@ public class RegisterActivity extends AppCompatActivity {
     @BindView(R.id.til_password) TextInputLayout tilPassword;
     ApiService service;
     Call<AccessToken> call;
+    AwesomeValidation validator;
+    TokenManager tokenManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,9 @@ public class RegisterActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         service = RetrofitBuilder.createService(ApiService.class);
-
+        validator = new AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT);
+        tokenManager = TokenManager.getInstance(getSharedPreferences("prefs",MODE_PRIVATE));
+        setupRules();
     }
 
     @OnClick(R.id.btn_register)
@@ -53,30 +62,34 @@ public class RegisterActivity extends AppCompatActivity {
         tilEmail.setError(null);
         tilPassword.setError(null);
 
-        call = service.register(name,email,password);
+        validator.clear();
 
-        call.enqueue(new Callback<AccessToken>(){
-            @Override
-            public void onResponse(Call<AccessToken> call, Response<AccessToken> response){
+        if(validator.validate()) {
+            call = service.register(name, email, password);
+            call.enqueue(new Callback<AccessToken>() {
+                @Override
+                public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
 
-                Log.w(TAG,"onResponse: "+response);
+                    Log.w(TAG, "onResponse: " + response);
 
-                if(response.isSuccessful()){
-
-                }else{
-                    handleErrors(response.errorBody());
+                    if (response.isSuccessful()) {
+                        Log.w(TAG,"onResponse: "+response.body());
+                        tokenManager.saveToken(response.body());
+                    } else {
+                        handleErrors(response.errorBody());
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<AccessToken> call, Throwable t){
-//                Log.w(TAG,"onFailure: "+t);
-                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
+                @Override
+                public void onFailure(Call<AccessToken> call, Throwable t) {
+                    Log.w(TAG, "onFailure: " + t);
+//                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+//                startActivity(intent);
+                }
 
 //            TODO lanjut streaming 34:25
-        });
+            });
+        }
     }
 
     private void handleErrors(ResponseBody response){
@@ -95,6 +108,13 @@ public class RegisterActivity extends AppCompatActivity {
                 tilPassword.setError(error.getValue().get(0));
             }
         }
+    }
+
+    public void setupRules(){
+        validator.addValidation(this,R.id.til_name, RegexTemplate.NOT_EMPTY,R.string.err_name);
+        validator.addValidation(this,R.id.til_email, Patterns.EMAIL_ADDRESS,R.string.err_email);
+        validator.addValidation(this,R.id.til_password, "[a-zA-Z0-9]{6,}",R.string.err_password);
+
     }
 
     @Override
